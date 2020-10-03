0592815273c41bfdeff6962a1c1a9160b645b9b5
Ryan Lugtu <ryanlugtu@Ryans-MacBook-Pro.local>
2020-10-03 03:55:00 -0400

    basic styling done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="headerContainer">
+        <div className="headerContent">
+          <h1 className="name type-animation">Ryan Lugtu.</h1>
+          <h3> Software Engineer</h3>
+          <h3>Fullstack Developer</h3>
+          <ul>
+            <li>LinkedIn</li>
+            <li>Github</li>
+            <li>Resume</li>
+            <li>Mail</li>
+          </ul>
+        </div>
+      </div>
+      <div className="aboutContainer">
+        <div className="aboutContent">
+          <h2 className="contentTitles">About</h2>
+          <p>lorem ipsum</p>
+          <div className="techStackContent">
+            <h2 className="contentTitles">Tech Stack</h2>
+            <p>
+              HTML, CSS, Javacript, Node.js, React.js, Express, Ruby, SQL,
+              PostgreSQL, Material UI
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="projectContainer">
+        <div className="projectsContent">
+          <h2 className="contentTitles">Projects</h2>
+          <p>
+            HTML, CSS, Javacript, Node.js, React.js, Express, Ruby, SQL,
+            PostgreSQL, Material UI
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
