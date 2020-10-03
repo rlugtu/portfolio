b875116f5f2391a414dbd0f6a5f42bcfc587cd61
Ryan Lugtu <ryanlugtu@Ryans-MacBook-Pro.local>
2020-10-03 14:02:46 -0400

    contact icons work

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import { makeStyles } from "@material-ui/core/styles";
-
+import Tooltip from "@material-ui/core/Tooltip";
+import { Linking } from "react";
 const useStyles = makeStyles((theme) => ({
   contactIcon: {
     fontSize: 80,
+    cursor: "pointer",
     [theme.breakpoints.down("xs")]: {
       fontSize: 60,
     },
@@ -26,23 +28,55 @@ function App() {
           <h3> Software Engineer</h3>
           <h3>Fullstack Developer</h3>
           <ul className="contactInfo">
-            <LinkedInIcon className={classes.contactIcon}></LinkedInIcon>
-            <GitHubIcon className={classes.contactIcon}></GitHubIcon>
-            <EmailIcon className={classes.contactIcon}></EmailIcon>
-            <AssignmentIcon className={classes.contactIcon}></AssignmentIcon>
+            <Tooltip title="LinkedIn" hover="pointer">
+              <a href="https://www.linkedin.com/in/ryan-lugtu/" target="_blank">
+                <LinkedInIcon className={classes.contactIcon}></LinkedInIcon>
+              </a>
+            </Tooltip>
+            <Tooltip title="GitHub">
+              <a href="https://github.com/rlugtu" target="_blank">
+                <GitHubIcon className={classes.contactIcon}></GitHubIcon>
+              </a>
+            </Tooltip>
+            <Tooltip title="Email">
+              <a href="mailto:ryanlugtu@gmail.com?subject=">
+                <EmailIcon className={classes.contactIcon}></EmailIcon>
+              </a>
+            </Tooltip>
+
+            <Tooltip title="Resume">
+              <AssignmentIcon className={classes.contactIcon}></AssignmentIcon>
+            </Tooltip>
           </ul>
         </div>
       </div>
       <div className="aboutContainer">
         <div className="aboutContent">
           <h2 className="contentTitles">About</h2>
-          <p>lorem ipsum</p>
-          <div className="techStackContent">
-            <h2 className="contentTitles">Tech Stack</h2>
-            <p>
-              HTML, CSS, Javacript, Node.js, React.js, Express, Ruby, SQL,
-              PostgreSQL, Material UI
-            </p>
+          <p>
+            Hello! I'm Ryan, a software engineer and full-stack developer. With
+            a passion for creation and a fascination for coding, I joined
+            General Assembly's Software Engineering Immersive Program. Following
+            the completion of over 500 hours of learning full-stack development,
+            I could not feel more fulfilled. I have a thirst for knowledge and
+            I'm driven to improve at what I do. When I'm not at my computer, you
+            can find me working out, working on my car, or playing tennis.
+          </p>
+          <h2 className="contentTitles">Tech Stack</h2>
+          <div className="techStackIcons">
+            <div className="techStackContainer"></div>
+            <i className="devicon-html5-plain-wordmark devicon"></i>
+            <i className="devicon-css3-plain-wordmark devicon"></i>
+            <i className="devicon-javascript-plain devicon"></i>
+            <i class="devicon-nodejs-plain-wordmark devicon"></i>
+            <i className="devicon-react-original-wordmark devicon"></i>
+            <i className="devicon-express-original-wordmark devicon"></i>
+            <i className="devicon-ruby-plain-wordmark devicon"></i>
+            <img
+              className="sqlIcon"
+              src="https://www.flaticon.com/svg/static/icons/svg/29/29594.svg"
+            />
+            <i className="devicon-postgresql-plain-wordmark devicon"></i>
           </div>
         </div>
       </div>
---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li>About</li>
+      <li>Portfolio</li>
+      <li></li>
+    </ul>
+  );
+};
+
+export default Navbar;
